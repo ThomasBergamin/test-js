948e07af2179d34527f095627d38a5cb5c62b9fe
Thomas <bergamin.tom@gmail.com>
2021-03-15 15:54:30 +0100

    pass calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,24 +1,46 @@
-function add () {
-	
+function add (num1, num2) {
+	return num1 + num2;
 }
 
-function subtract () {
-	
+function subtract (num1, num2) {
+	return num1 - num2;
 }
 
-function sum () {
-	
+function sum (params) {
+	let sum = 0
+	for(const number of params){
+		sum += number;
+	}
+	return sum
 }
 
-function multiply () {
-	
+function multiply (params) {
+	let total = 1
+	for(const number of params){
+		total *= number;
+	}
+	return total
 }
 
-function power() {
-	
+function power(num1, num2) {
+	return Math.pow(num1, num2)
 }
 
-function factorial() {
+function factorial(num) {
+	if(num===0){
+		return 1;
+	} else {
+		let numbersBelow = [];
+		for(i=num-1; i>0; i--){
+			numbersBelow.unshift(i);
+		}
+		let total = num;
+	
+		for(const number of numbersBelow){
+			total *= number;
+		}
+		return total;
+	}
 	
 }
 
