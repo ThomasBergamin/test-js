a109e4eb6921e7409ca3bb13eb7dd75193864feb
Thomas <bergamin.tom@gmail.com>
2021-03-15 19:52:22 +0100

    pass palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,39 @@
-const palindromes = function() {
+const palindromes = function(string) {
 
+    // Lower all characters
+
+    let lowercaseString = string.toLowerCase();
+
+    // Remove punctuations
+
+    let regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+
+    var cleanString = lowercaseString.replace(regex, "")
+
+    // Remove white space
+
+    var stringToReverse = cleanString.split(" ").join("");
+
+    // Create a variable with the reversed string
+
+    let splittedString = stringToReverse.split("");
+    let listReversedString = [];
+
+    for(const letter of splittedString){
+        listReversedString.unshift(letter)
+    }
+
+    reversedString = listReversedString.join("")
+
+    // Compare both strings
+
+    if(reversedString === stringToReverse){
+        return true;
+    } else {
+        return false;
+    }
+    
 }
 
 module.exports = palindromes
+
