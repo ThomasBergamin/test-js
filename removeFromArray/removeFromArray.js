91b21ab649fda28a991c12c4d10f196f62750a27
Thomas <bergamin.tom@gmail.com>
2021-03-15 22:25:03 +0100

    pass removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...argsToRemove) {
+
+    // Loop over all args
+    for(arg of argsToRemove){
+
+        // Check if they are in array, else ignore
+        if (array.includes(arg)){
+            // Remove them in array
+            let index = array.indexOf(arg);
+            array.splice(index, 1);
+        }
+        
+    }
+    return array
 
 }
 
