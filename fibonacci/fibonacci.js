9c6d71e98b686a7ed0747b7f1c20b5bf3d133e0a
Thomas <bergamin.tom@gmail.com>
2021-03-15 16:12:56 +0100

    pass fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    number = parseInt(number)
+    if(number===0){
+        return(0);
+    }
+    else if(number===1){
+        return(1);
+    }
+    else if(number < 0){
+        return("OOPS");
+    }
+    else if(number > 1){
+        let listNumber = [1, 1];
 
+        while(listNumber.length < number){
+            let newNumber = listNumber[listNumber.length-1] + listNumber[listNumber.length-2];
+            listNumber.push(newNumber);
+        }
+        return(listNumber[listNumber.length-1]);
+    }
 }
+
 
 module.exports = fibonacci
