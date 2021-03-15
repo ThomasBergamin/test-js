460949a30d4df60e48f45d8fc45c6653f313c8e2
Thomas <bergamin.tom@gmail.com>
2021-03-15 19:37:45 +0100

    pass tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temperature) {
+  let temperatureConverted = (temperature - 32) * 5/9;
+  let temperatureToReturn = Math.round(temperatureConverted * 10) / 10;
+  return temperatureToReturn;
 }
 
-const ctof = function() {
-
+const ctof = function(temperature) {
+  let temperatureConverted = temperature * 9/5 + 32;
+  let temperatureToReturn = Math.round(temperatureConverted * 10) / 10;
+  return temperatureToReturn;
 }
 
 module.exports = {
