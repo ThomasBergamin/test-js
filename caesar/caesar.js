91c90299ba864c48dc696f650eb4cf462276e994
Thomas <bergamin.tom@gmail.com>
2021-03-16 12:44:15 +0100

    add explanation for caesar shift

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -36,9 +36,14 @@ const caesar = function(string, shift) {
 
         if(charInUnicode >= 65 && charInUnicode <= 90 || charInUnicode >= 97 && charInUnicode <= 122){
 
-            // Case where we have a upper case letter : ex char B (66) and shift 28 => 66 - 65 + 28 = 29 % 26 = 3 + 65 = 68 (D)
+            // Case where we have a upper case letter : 
 
             if(charInUnicode >= 65 && charInUnicode <= 90){
+
+                // Refactor the shift in case it is higher than 26 : ex char B (66) and shift 28 => 66 - 65 + 28 = 29 % 26 = 3 + 65 = 68 (D)
+
+                // charInUnicode - 65 so we reset unicode back to 0, A = 0, B = 1 ...
+                // % 26 so we cut useless multiples of 26 in the shift
 
                 encodedChar = String.fromCharCode(((charInUnicode - 65 + shift) % 26) + 65);
 
