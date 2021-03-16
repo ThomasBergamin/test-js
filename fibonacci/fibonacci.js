46521a1a1d1967330be33641cd9bb030006b4b03
Thomas <bergamin.tom@gmail.com>
2021-03-16 13:52:55 +0100

    fix typos

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -6,13 +6,22 @@ const fibonacci = function(number) {
     else if(number===1){
         return(1);
     }
+
+    // Cannot input negative numbers
     else if(number < 0){
         return("OOPS");
     }
+
     else if(number > 1){
+
+        // Init the beginning of the fibonacci sequence
         let listNumber = [1, 1];
 
+        // Append numbers in the fibonacci sequence until the length of the list is equal to the input
+
         while(listNumber.length < number){
+            
+            // Add 2 last numbers in the list then add this number at the end of the list
             let newNumber = listNumber[listNumber.length-1] + listNumber[listNumber.length-2];
             listNumber.push(newNumber);
         }
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,8 +1,10 @@
 const reverseString = function(string) {
 
+    // Split word(s) in a list of char
     let splittedString = string.split("");
     let reversedString = [];
 
+    // Loop each char and at at the beginning of new list
     for(const letter of splittedString){
         reversedString.unshift(letter)
     }
